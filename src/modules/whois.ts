890ea50107eb0c74dded1b653695ee712f9a049c
ArnabXD <arnabxd@pm.me>
2022-01-29 14:36:19 +0530

    refactor: reduce usage of `as`

--- src/modules/whois.ts
+++ src/modules/whois.ts
@@ -5,27 +5,31 @@ import { escape } from 'html-escaper'
 
 const whois: Module = {
 	handlers: [
-		new CommandHandler('whois', async (client, event, args) => {
+		new CommandHandler('whois', async (client, event, _args, input) => {
 			let userId: bigInt.BigInteger | string = 'me'
 
 			// In private chats show details of the person you are talking to
-			if (event.isPrivate) {
-				userId = event.chatId as bigInt.BigInteger
+			if (event.isPrivate && event.chatId) {
+				userId = event.chatId
 			}
 
 			/**
 			 * Additionally userid/username can be used
 			 * @example >whois 777000
 			 */
-			if (args.length) {
-				userId = args[0]
+			if (input) {
+				userId = input
 			}
 
 			// Get info of user by replying to his/her message
 			if (!event.isPrivate && event.message.replyToMsgId) {
 				const repliedToMessage = await event.message.getReplyMessage()
-				if (repliedToMessage) {
-					userId = (repliedToMessage.fromId as Api.PeerUser).userId
+				if (
+					repliedToMessage &&
+					repliedToMessage.fromId &&
+					'userId' in repliedToMessage.fromId
+				) {
+					userId = repliedToMessage.fromId.userId
 				}
 			}
 
@@ -33,21 +37,30 @@ const whois: Module = {
 			const { fullUser, users } = await client.invoke(
 				new Api.users.GetFullUser({ id: userEntity })
 			)
-			const user = users[0] as Api.User
+			const user = users[0]
 
-			await event.message.edit({
-				text:
-					`<a href="tg://user?id=${user.id}"><b>${escape(
-						user.firstName as string
-					)} ${escape(user.lastName || '')}</b></a>\n` +
-					`<b>ID:</b> <code>${user.id}</code>\n` +
-					`<b>DC:</b> ${
-						user.photo && 'dcId' in user.photo ? user.photo.dcId : 'N/A'
-					}\n` +
-					`<b>Username:</b> ${user.username ? '@' + user.username : 'N/A'}\n` +
-					`<b>Description:</b> ${fullUser.about || 'N/A'}`,
-				parseMode: 'html'
-			})
+			// Check if type of user is Api.User
+			if ('firstName' in user) {
+				await event.message.edit({
+					text:
+						`<a href="tg://user?id=${user.id}"><b>${escape(
+							user.firstName || ''
+						)} ${escape(user.lastName || '')}</b></a>\n` +
+						`<b>ID:</b> <code>${user.id}</code>\n` +
+						`<b>DC:</b> ${
+							user.photo && 'dcId' in user.photo ? user.photo.dcId : 'N/A'
+						}\n` +
+						`<b>Username:</b> ${
+							user.username ? '@' + user.username : 'N/A'
+						}\n` +
+						`<b>Description:</b> ${fullUser.about || 'N/A'}`,
+					parseMode: 'html'
+				})
+			}
+			// If typeof user is UserEmpty
+			else {
+				await event.message.edit({ text: 'Invalid id/username' })
+			}
 		})
 	]
 }
