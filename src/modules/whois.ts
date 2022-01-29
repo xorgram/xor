79948279a8d5fe379b5f1adee3ab0c28dbfbc88a
ArnabXD <arnabxd@pm.me>
2022-01-29 15:13:05 +0530

    fix: whois of userid/username

--- src/modules/whois.ts
+++ src/modules/whois.ts
@@ -5,7 +5,7 @@ import { escape } from 'html-escaper'
 
 const whois: Module = {
 	handlers: [
-		new CommandHandler('whois', async (client, event, _args, input) => {
+		new CommandHandler('whois', async (client, event, args) => {
 			let userId: bigInt.BigInteger | string = 'me'
 
 			// In private chats show details of the person you are talking to
@@ -17,8 +17,8 @@ const whois: Module = {
 			 * Additionally userid/username can be used
 			 * @example >whois 777000
 			 */
-			if (input) {
-				userId = input
+			if (args.length) {
+				userId = args[0]
 			}
 
 			// Get info of user by replying to his/her message
@@ -33,34 +33,29 @@ const whois: Module = {
 				}
 			}
 
-			const userEntity = await client.getEntity(userId)
-			const { fullUser, users } = await client.invoke(
-				new Api.users.GetFullUser({ id: userEntity })
+			const user = await client.getEntity(userId)
+			// In case of id is invalid or id isn't an userid
+			if (!user || !('firstName' in user)) {
+				await event.message.edit({ text: `Invalid ID` })
+				return
+			}
+			const { fullUser } = await client.invoke(
+				new Api.users.GetFullUser({ id: user.id })
 			)
-			const user = users[0]
 
-			// Check if type of user is Api.User
-			if ('firstName' in user) {
-				await event.message.edit({
-					text:
-						`<a href="tg://user?id=${user.id}"><b>${escape(
-							user.firstName || ''
-						)} ${escape(user.lastName || '')}</b></a>\n` +
-						`<b>ID:</b> <code>${user.id}</code>\n` +
-						`<b>DC:</b> ${
-							user.photo && 'dcId' in user.photo ? user.photo.dcId : 'N/A'
-						}\n` +
-						`<b>Username:</b> ${
-							user.username ? '@' + user.username : 'N/A'
-						}\n` +
-						`<b>Description:</b> ${fullUser.about || 'N/A'}`,
-					parseMode: 'html'
-				})
-			}
-			// If typeof user is UserEmpty
-			else {
-				await event.message.edit({ text: 'Invalid id/username' })
-			}
+			await event.message.edit({
+				text:
+					`<a href="tg://user?id=${user.id}"><b>${escape(
+						user.firstName || ''
+					)} ${escape(user.lastName || '')}</b></a>\n` +
+					`<b>ID:</b> <code>${user.id}</code>\n` +
+					`<b>DC:</b> ${
+						user.photo && 'dcId' in user.photo ? user.photo.dcId : 'N/A'
+					}\n` +
+					`<b>Username:</b> ${user.username ? '@' + user.username : 'N/A'}\n` +
+					`<b>Description:</b> ${fullUser.about || 'N/A'}`,
+				parseMode: 'html'
+			})
 		})
 	]
 }
