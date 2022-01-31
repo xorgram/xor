285387695b514fbde604d631f585f7946d93eb1f
Roj <ez@roj.im>
2022-01-31 15:58:38 +0300

    feat: make `installModules` install from a directory (#22)

--- src/module.ts
+++ src/module.ts
@@ -1,3 +1,6 @@
+import { join } from 'path'
+import { readdirSync } from 'fs'
+
 import { TelegramClient } from 'telegram'
 import { NewMessage, NewMessageEvent } from 'telegram/events'
 
@@ -15,7 +18,16 @@ export async function wrap(func: () => Promise<void>) {
 	}
 }
 
-export function installModules(client: TelegramClient, modules: Module[]) {
+export async function installModules(client: TelegramClient, dir: string) {
+	const modules = new Array<Module>()
+	const files = readdirSync(dir)
+	for (let file of files) {
+		file =
+			file.endsWith('.ts') || file.endsWith('.js') ? file.slice(0, -3) : file
+		const spec = join(dir, file)
+		const mod = await import(spec)
+		if (mod.default !== undefined) modules.push(mod.default)
+	}
 	async function eventHandler(event: NewMessageEvent) {
 		for (const { handlers } of modules)
 			for (const handler of handlers)
