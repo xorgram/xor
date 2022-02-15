815a9acb9c2c04cf11fcb88105725386c28a43d6
ArnabXD <arnabxd@pm.me>
2022-02-15 23:11:41 +0530

    feat: ban command

--- src/modules/admin/helpers.ts
+++ src/modules/admin/helpers.ts
@@ -1,6 +1,11 @@
 import { Api, TelegramClient } from 'telegram'
+import { NewMessageEvent } from 'telegram/events'
 
-export const isAdmin = async (
+export const amIAdmin = (chat: Api.TypeEntityLike) => {
+	return chat instanceof Api.Channel && !chat.adminRights
+}
+
+export const isUserAdmin = async (
 	user: Api.User,
 	chat: Api.TypeEntityLike,
 	client: TelegramClient
@@ -18,4 +23,132 @@ export const isAdmin = async (
 		participant instanceof Api.ChannelParticipantAdmin ||
 		participant instanceof Api.ChannelParticipantCreator
 	)
+}
+
+export const getUser = async (
+	event: NewMessageEvent,
+	client: TelegramClient,
+	args: string[],
+	getRank = false
+) => {
+	let entity: Api.User = {} as Api.User
+	let rank = 'Admin'
+
+	const reply = await event.message.getReplyMessage()
+	if (reply) {
+		const sender = await reply.getSender()
+		if (sender && sender instanceof Api.User) {
+			entity = sender
+		}
+		if (getRank && args[0]) {
+			rank = args[0]
+		}
+	} else if (args[0] !== undefined && args[0].length != 0) {
+		const _entity = await client.getEntity(args[0])
+		if (!(_entity instanceof Api.User)) {
+			await event.message.edit({ text: 'Invalid username/user id' })
+			return
+		}
+		entity = _entity
+		if (getRank && args[1]) {
+			rank = args[1]
+		}
+	}
+
+	return {
+		entity,
+		rank: rank
+	}
+}
+
+export const promote = async (
+	user: Api.User,
+	rank: string,
+	chat: Api.Channel,
+	client: TelegramClient
+) => {
+	await client.invoke(
+		new Api.channels.EditAdmin({
+			channel: chat.id,
+			userId: user,
+			adminRights: new Api.ChatAdminRights({
+				addAdmins: chat.adminRights?.addAdmins ?? false,
+				anonymous: chat.adminRights?.anonymous ?? false,
+				banUsers: chat.adminRights?.banUsers ?? false,
+				changeInfo: chat.adminRights?.changeInfo ?? false,
+				deleteMessages: chat.adminRights?.deleteMessages ?? false,
+				editMessages: chat.adminRights?.deleteMessages ?? false,
+				inviteUsers: chat.adminRights?.inviteUsers ?? false,
+				manageCall: chat.adminRights?.manageCall ?? false,
+				pinMessages: chat.adminRights?.pinMessages ?? false,
+				postMessages: chat.adminRights?.postMessages ?? false,
+				other: chat.adminRights?.other ?? false
+			}),
+			rank
+		})
+	)
+}
+
+export const demote = async (
+	user: Api.User,
+	chat: Api.Channel,
+	client: TelegramClient
+) => {
+	await client.invoke(
+		new Api.channels.EditAdmin({
+			channel: chat.id,
+			userId: user,
+			adminRights: new Api.ChatAdminRights({
+				addAdmins: false,
+				anonymous: false,
+				banUsers: false,
+				changeInfo: false,
+				deleteMessages: false,
+				editMessages: false,
+				inviteUsers: false,
+				manageCall: false,
+				pinMessages: false,
+				postMessages: false,
+				other: false
+			}),
+			rank: ''
+		})
+	)
+}
+
+export const banUser = async (
+	user: Api.User,
+	chat: Api.TypeEntityLike,
+	client: TelegramClient
+) => {
+	if (chat instanceof Api.Channel) {
+		if (chat.gigagroup || chat.megagroup) {
+			return await client.invoke(
+				new Api.channels.EditBanned({
+					channel: chat,
+					participant: user,
+					bannedRights: new Api.ChatBannedRights({
+						untilDate: 0,
+						viewMessages: true,
+						sendMessages: true,
+						sendMedia: true,
+						sendStickers: true,
+						sendGifs: true,
+						sendGames: true,
+						sendInline: true,
+						embedLinks: true
+					})
+				})
+			)
+		}
+
+		console.log(chat.id)
+
+		return await client.invoke(
+			new Api.messages.DeleteChatUser({
+				chatId: chat.id,
+				userId: user.id
+			})
+		)
+	}
 }
--- src/modules/admin/index.ts
+++ src/modules/admin/index.ts
@@ -2,7 +2,14 @@ import { Api } from 'telegram'
 import { sleep } from 'telegram/Helpers'
 import { CommandHandler } from '../../handlers'
 import { Module } from '../../module'
-import { isAdmin } from './helpers'
+import {
+	amIAdmin,
+	isUserAdmin,
+	promote,
+	demote,
+	banUser,
+	getUser
+} from './helpers'
 
 const admin: Module = {
 	name: 'admin',
@@ -14,36 +21,18 @@ const admin: Module = {
 				return
 			}
 
-			if (chat instanceof Api.Channel && !chat.adminRights) {
+			if (!amIAdmin(chat)) {
 				await event.message.edit({ text: 'I am not an admin here' })
 				return
 			}
 
-			let entity: Api.User = {} as Api.User
-			let rank = 'Admin'
-
-			const reply = await event.message.getReplyMessage()
-			if (reply) {
-				const sender = await reply.getSender()
-				if (sender && sender instanceof Api.User) {
-					entity = sender
-				}
-				if (args[0]) {
-					rank = args[0]
-				}
-			} else if (args[0] !== undefined && args[0].length != 0) {
-				const _entity = await client.getEntity(args[0])
-				if (!(_entity instanceof Api.User)) {
-					await event.message.edit({ text: 'Invalid username/user id' })
-					return
-				}
-				entity = _entity
-				if (args[1]) {
-					rank = args[1]
-				}
+			const user = await getUser(event, client, args, true)
+			if (!user) {
+				await event.message.edit({ text: 'User not found' })
+				return
 			}
 
-			if (await isAdmin(entity, chat, client)) {
+			if (await isUserAdmin(user.entity, chat, client)) {
 				await event.message.edit({
 					text: "User is already an admin, Can't promote him/her"
 				})
@@ -51,27 +40,8 @@ const admin: Module = {
 			}
 
 			if (chat instanceof Api.Channel && chat.adminRights) {
-				await client.invoke(
-					new Api.channels.EditAdmin({
-						channel: chat.id,
-						userId: entity,
-						adminRights: new Api.ChatAdminRights({
-							addAdmins: chat.adminRights.addAdmins,
-							anonymous: chat.adminRights.anonymous,
-							banUsers: chat.adminRights.banUsers,
-							changeInfo: chat.adminRights.changeInfo,
-							deleteMessages: chat.adminRights.deleteMessages,
-							editMessages: chat.adminRights.deleteMessages,
-							inviteUsers: chat.adminRights.inviteUsers,
-							manageCall: chat.adminRights.manageCall,
-							pinMessages: chat.adminRights.pinMessages,
-							postMessages: chat.adminRights.postMessages,
-							other: chat.adminRights.other
-						}),
-						rank
-					})
-				)
-				await event.message.edit({ text: 'Promoted to ' + rank })
+				await promote(user.entity, user.rank, chat, client)
+				await event.message.edit({ text: 'Promoted to admin' })
 			} else {
 				await event.message.edit({ text: 'I have no admin rights.' })
 				await sleep(2500)
@@ -85,60 +55,56 @@ const admin: Module = {
 				return
 			}
 
-			if (chat instanceof Api.Channel && !chat.adminRights) {
+			if (!amIAdmin(chat)) {
 				await event.message.edit({ text: 'I am not an admin here' })
 				return
 			}
 
-			let entity: Api.User = {} as Api.User
-
-			const reply = await event.message.getReplyMessage()
-			if (reply) {
-				const sender = await reply.getSender()
-				if (sender && sender instanceof Api.User) {
-					entity = sender
-				}
-			} else if (args[0] !== undefined && args[0].length != 0) {
-				const _entity = await client.getEntity(args[0])
-				if (!(_entity instanceof Api.User)) {
-					await event.message.edit({ text: 'Invalid username/user id' })
-					return
-				}
-				entity = _entity
+			const user = await getUser(event, client, args)
+			if (!user) {
+				await event.message.edit({ text: 'User not found' })
+				return
 			}
 
-			if (!(await isAdmin(entity, chat, client))) {
+			if (!(await isUserAdmin(user.entity, chat, client))) {
 				await event.message.edit({ text: "User isn't an admin" })
 				return
 			}
 
 			if (chat instanceof Api.Channel && chat.adminRights) {
-				await client.invoke(
-					new Api.channels.EditAdmin({
-						channel: chat.id,
-						userId: entity,
-						adminRights: new Api.ChatAdminRights({
-							addAdmins: false,
-							anonymous: false,
-							banUsers: false,
-							changeInfo: false,
-							deleteMessages: false,
-							editMessages: false,
-							inviteUsers: false,
-							manageCall: false,
-							pinMessages: false,
-							postMessages: false,
-							other: false
-						}),
-						rank: ''
-					})
-				)
+				await demote(user.entity, chat, client)
 				await event.message.edit({ text: 'Demoted successfully' })
 			} else {
 				await event.message.edit({ text: "Can't demote this user" })
 				await sleep(3000)
 				await event.message.delete({ revoke: true })
 			}
+		}),
+		new CommandHandler('ban', async (client, event, args) => {
+			const chat = await event.message.getChat()
+			if (!chat) {
+				await event.message.edit({ text: "Couldn't fetch chat info" })
+				return
+			}
+
+			if (chat instanceof Api.Channel && !chat.adminRights) {
+				await event.message.edit({ text: 'I am not an admin here' })
+				return
+			}
+
+			const user = await getUser(event, client, args)
+			if (!user) {
+				await event.message.edit({ text: 'User not found' })
+				return
+			}
+
+			if (await isUserAdmin(user.entity, chat, client)) {
+				await event.message.edit({ text: 'User is an admin' })
+				return
+			}
+
+			await banUser(user.entity, chat, client)
+			await event.message.edit({ text: 'Banned successully' })
 		})
 	],
 	help: `
@@ -155,6 +121,10 @@ Promotes user to admin. Format : 'promote @username custom-title'. Or if you rep
 - demote
 
 Demotes participant from admin to user. Format : 'demote @username/userid'. Or if you reply to someone's message - 'demote'
+
+- ban
+
+Bans or removes user from chat. Format : 'ban username/user id' or reply 'ban' to someone's message
 `
 }
 
