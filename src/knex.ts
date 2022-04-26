c7b0d3aab4f37baf79de3920d2f80bb55d9312b0
ArnabXD <arnabxd@pm.me>
2022-04-26 15:06:32 +0530

    refactor: class to function apporach

--- src/knex.ts
+++ src/knex.ts
@@ -1,21 +1,16 @@
 import { knex as _knex, Knex } from 'knex'
 import env from './env'
 
-export default class SingletonKnex {
-	private static instance: Knex
-	private constructor() {
-		//
+let instance: Knex | undefined
+export default function knex() {
+	if (!env.DATABASE_URI) {
+		throw new Error('DATABASE_URI variable not found')
 	}
-	public static getInstance(): Knex {
-		if (!env.DATABASE_URI) {
-			throw new Error('DATABASE_URI variable not found')
-		}
-		if (!SingletonKnex.instance) {
-			SingletonKnex.instance = _knex({
-				client: 'pg',
-				connection: env.DATABASE_URI
-			})
-		}
-		return SingletonKnex.instance
+	if (instance === undefined) {
+		instance = _knex({
+			client: 'pg',
+			connection: env.DATABASE_URI
+		})
 	}
+	return instance
 }
