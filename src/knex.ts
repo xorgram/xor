ad4f78e4d5a1e4793714d1308a70dcca5353c54a
ArnabXD <arnabxd@pm.me>
2022-04-30 00:31:46 +0530

    chore: back to singleton strategy

--- src/knex.ts
+++ src/knex.ts
@@ -1,16 +1,23 @@
 import { knex as _knex, Knex } from 'knex'
 import env from './env'
 
-let instance: Knex | undefined
-export default function knex() {
-	if (!env.DATABASE_URI) {
-		throw new Error('DATABASE_URI variable not found')
+const { Instance } = class SingletonKnex {
+	private static instance: Knex
+	private constructor() {
+		//
 	}
-	if (instance === undefined) {
-		instance = _knex({
-			client: 'pg',
-			connection: env.DATABASE_URI
-		})
+	public static get Instance(): Knex {
+		if (!env.DATABASE_URI) {
+			throw new Error('DATABASE_URI variable not found')
+		}
+		if (!SingletonKnex.instance) {
+			SingletonKnex.instance = _knex({
+				client: 'pg',
+				connection: env.DATABASE_URI
+			})
+		}
+		return SingletonKnex.instance
 	}
-	return instance
 }
+
+export default Instance
