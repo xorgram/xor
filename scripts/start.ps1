13af3f8a0096f9a700e0fb0beac453dbdfa9e57e
ArnabXD <arnabxd@pm.me>
2022-05-06 23:15:32 +0530

    chore: remove unused variable latest tag

--- scripts/start.ps1
+++ scripts/start.ps1
@@ -1,7 +1,6 @@
 $ARCHIVE_NAME = "archive.tgz"
-$LATEST_TAG = $(Invoke-RestMethod -Uri "https://api.github.com/repos/xorgram/xor/releases/latest").tag_name
 
-Invoke-WebRequest -Uri "https://github.com/xorgram/xor/releases/download/$LATEST_TAG/$ARCHIVE_NAME" -OutFile $ARCHIVE_NAME
+Invoke-WebRequest -Uri "https://github.com/xorgram/xor/releases/download/latest/$ARCHIVE_NAME" -OutFile $ARCHIVE_NAME
 tar xf $ARCHIVE_NAME
 Remove-Item $ARCHIVE_NAME
 if (Test-Path "dist") {
