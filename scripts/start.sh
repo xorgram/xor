3e11f272787ee52373841718e34316685ca0d0b1
Arnab Paryali <arnabxd@pm.me>
2022-05-07 21:28:17 +0530

    chore: remove unused variable latest tag (#168)

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,8 +1,6 @@
 ARCHIVE_NAME=archive.tgz
-# https://gist.github.com/lukechilds/a83e1d7127b78fef38c2914c4ececc3c
-LATEST_TAG=$(wget -qO- https://api.github.com/repos/xorgram/xor/releases/latest | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
 
-wget https://github.com/xorgram/xor/releases/download/$LATEST_TAG/$ARCHIVE_NAME -O $ARCHIVE_NAME
+wget https://github.com/xorgram/xor/releases/latest/download/$ARCHIVE_NAME -O $ARCHIVE_NAME
 tar xf $ARCHIVE_NAME
 rm $ARCHIVE_NAME
 rm -rf dist
