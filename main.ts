ea1c4f8158f2a6bf46df59fb8aa55c4115183682
Roj <ez@roj.im>
2022-07-16 15:30:44 +0300

    refactor: use grm 0.1.6 syntax

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import { events, LogLevel, StringSession, TelegramClient } from "$grm";
+import { events, StringSession, TelegramClient } from "$grm";
 import { dirname, fromFileUrl, join } from "./deps.ts";
 import env from "./env.ts";
 import { managerModule, ModuleManager } from "./module_manager.ts";
@@ -7,11 +7,10 @@ const client = new TelegramClient(
   new StringSession(env.STRING_SESSION),
   env.APP_ID,
   env.APP_HASH,
-  {},
+  { logLevel: "none" },
 );
 const manager = new ModuleManager(client);
 client.setParseMode(undefined);
-client.setLogLevel(LogLevel.NONE);
 try {
   await Deno.mkdir("externals");
 } catch (_err) {
@@ -26,4 +25,4 @@ manager.installMultiple(
 manager.install(managerModule(manager), false);
 manager.installMultiple(await ModuleManager.directory("externals"), true);
 client.addEventHandler(manager.handler, new events.NewMessage({}));
-client.start({ botAuthToken: "" });
+client.start();
