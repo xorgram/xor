1f2b53dc66f8a43cf2e3be425d858a9763a08419
Dunkan <70066170+dcdunkan@users.noreply.github.com>
2022-07-21 18:04:30 +0530

    fix: resolve to absolute path

--- deps.ts
+++ deps.ts
@@ -5,6 +5,7 @@ export {
   dirname,
   fromFileUrl,
   join,
+  resolve,
   toFileUrl,
-} from "https://deno.land/std@0.148.0/path/mod.ts";
+} from "https://deno.land/std@0.149.0/path/mod.ts";
 export * from "https://deno.land/x/grm_parse@0.0.3/mod.ts";
