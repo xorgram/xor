002da2788cf1ee76c12d0284b8a5ca14151f5346
Roj <ez@roj.im>
2022-07-21 16:35:24 +0300

    chore: use Deno.stdout.write instead of console.log

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,2 +1,2 @@
-BASE_URL=https://raw.githubusercontent.com/xorgram/xor/$(deno eval "console.log((await (await fetch(\"https://api.github.com/repos/xorgram/xor/tags\")).json())[0].name)")
+BASE_URL=https://raw.githubusercontent.com/xorgram/xor/$(deno eval "await Deno.stdout.write(new TextEncoder().encode((await (await fetch(\"https://api.github.com/repos/xorgram/xor/tags\")).json())[0].name))")
 deno run --allow-env --allow-net --allow-read --allow-run --allow-write --import-map=$BASE_URL/import_map.json -r $BASE_URL/main.ts
