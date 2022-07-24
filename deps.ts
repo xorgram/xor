1f8278010f5b01aa5245f6236b4dd46090d6ed52
Roj <ez@roj.im>
2022-07-24 21:51:45 +0300

    chore: bump deps

--- deps.ts
+++ deps.ts
@@ -8,4 +8,4 @@ export {
   resolve,
   toFileUrl,
 } from "https://deno.land/std@0.149.0/path/mod.ts";
-export * from "https://deno.land/x/grm_parse@0.0.3/mod.ts";
+export * from "https://deno.land/x/grm_parse@0.0.4/mod.ts";
