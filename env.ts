4d3bab1b58b7163e668f59c099c4c19aae23c8ab
ArnabXD <arnabxd@proton.me>
2022-07-29 12:49:04 +0530

    feat: allow COMMAND & INPUT prefix customization

--- env.ts
+++ env.ts
@@ -1,9 +1,37 @@
-import { cleanEnv, config, num, str } from "./deps.ts";
+import { cleanEnv, config, num, str, makeValidator } from "./deps.ts";
 
 await config({ export: true });
+
+// https://stackoverflow.com/a/54256858/12250600
+const PREFIX_REGEX = /^[^\p{L}\d\s@#\$]$/u;
+
+const cmdPrefix = makeValidator((input) => {
+  if (PREFIX_REGEX.test(input)) {
+    return input;
+  }
+  console.warn(
+    "COMMAND_PREFIX falling back to '\\', single symbol expected excluding @,#,$"
+  );
+  return "\\";
+});
+
+const inputPrefix = makeValidator((input) => {
+  if (
+    PREFIX_REGEX.test(input) &&
+    input !== Deno.env.toObject().COMMAND_PREFIX
+  ) {
+    return input;
+  }
+  console.warn(
+    "INPUT_PREFIX falling back to '>', single symbol expected excluding @,#,$ & COMMAND_PREFIX"
+  );
+  return "\\";
+});
 
 export default cleanEnv(Deno.env.toObject(), {
   STRING_SESSION: str(),
   APP_ID: num(),
   APP_HASH: str(),
+  COMMAND_PREFIX: cmdPrefix({ default: "\\" }),
+  INPUT_PREFIX: inputPrefix({ default: ">" }),
 });
