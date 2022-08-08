065684fc49248fbd8ace5db33899169914e376f2
Roj <ez@roj.im>
2022-08-08 15:47:49 +0300

    chore: ensure module names match `/^[a-z]{1,10}$/`

--- module.ts
+++ module.ts
@@ -11,9 +11,10 @@ export function isModule(value: unknown): value is Module {
   const obj = Object(value);
   return (
     typeof obj.name === "string" &&
+    /^[a-z]{1,10}$/.test(obj.name) &&
     Array.isArray(obj.handlers) &&
     obj.handlers.filter((handler: unknown) => handler instanceof Handler)
-        .length != undefined
+        .length != 0
   );
 }
 
