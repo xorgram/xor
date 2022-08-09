e65878d33b116debac545eb0428c128f48dfc813
Roj <ez@roj.im>
2022-08-09 11:44:43 +0300

    feat: persist `localStorage` (#239)

--- setup.ts
+++ setup.ts
@@ -1,5 +1,6 @@
 import * as log from "std/log/mod.ts";
 import { getLevelByName } from "std/log/levels.ts";
+import { setup } from "pls";
 
 const level = (() => {
   let toReturn = "INFO";
@@ -30,3 +31,20 @@ await log.setup({
     },
   },
 });
+
+let connected = false;
+
+if (Deno.env.get("PLS_CONNECTION_URI")) {
+  try {
+    await setup({ include: [/^module_/] });
+    connected = true;
+  } catch (err) {
+    log.warning(`could not connect to the database: ${err}`);
+  }
+}
+
+if (connected) {
+  log.info("connected to the database");
+} else {
+  log.info("not using database");
+}
