7ff1ca20349659aadb23eeb3c0042d8b16b48d5e
Roj <ez@roj.im>
2022-11-04 21:28:02 +0300

    chore: bump deps

--- deps.ts
+++ deps.ts
@@ -5,4 +5,4 @@ export {
   num,
   str,
 } from "https://deno.land/x/envalid@0.1.2/mod.ts";
-export * from "https://deno.land/x/grm_parse@0.0.10/mod.ts";
+export * from "https://deno.land/x/grm_parse@0.0.12/mod.ts";
